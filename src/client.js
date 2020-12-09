c09e958985b4ef820450cd1ed2f324ed5acce56b
Justin Cordero <justincordero@ymail.com>
2020-12-09 15:18:00 -0500

    fix sanity connection

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client';
+
+export default sanityClient({
+    projectId: "th6lbwxi",
+    dataset: "production"
+})
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ export default function NavBar() {
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink to='/' exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
-                        John Doe
+                        John Smith
                     </NavLink>
                     <NavLink to='/post' activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded tex-red-200 hover:text-green-800">
                         Blog
