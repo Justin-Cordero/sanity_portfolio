c7add8aed73a1149d8a3b0493f748188e45d469d
Justin Cordero <justincordero@ymail.com>
2020-12-10 19:55:46 -0500

    update styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,11 @@ import image from '../headerbg.jpeg';
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="scenic river" className="absolute object-cover w-full h-full"/>
+            <img src={image} alt="Scenic River" className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-72 px-8">
-                <h1 className="text-6xl text-red-100 font-bold cursive leading-none lg:leading-snug home-name">Hey there! I'm John Smith</h1>
+                <h1 className="text-6xl text-red-100 font-bold cursive leading-none lg:leading-snug home-name">
+                    John Smith.<br></br>Traveler & Photographer.
+                </h1>
             </section>
         </main>
     )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,20 +4,20 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
     return (
-        <header className='bg-red-600'>
+        <header className='bg-gray-300'>
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
-                    <NavLink to='/' exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
+                    <NavLink to='/' exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 text-gray-500 hover:text-gray-50 text-4xl font-bold cursive tracking-widest">
                         John Smith
                     </NavLink>
-                    <NavLink to='/post' activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded tex-red-200 hover:text-green-800">
-                        Blog
+                    <NavLink to='/about' activeClassName="text-gray-50 bg-gray-400" className="inline-flex items-center py-3 px-3 my-6 rounded tex-red-200 hover:text-gray-50">
+                        About
                     </NavLink>
-                    <NavLink to='/project' activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded tex-red-200 hover:text-green-800">
+                    <NavLink to='/project' activeClassName="text-gray-50 bg-gray-400" className="inline-flex items-center py-3 px-3 my-6 rounded tex-red-200 hover:text-gray-50">
                         Projects
                     </NavLink>
-                    <NavLink to='/about' activeClassName="text-red-100 bg-red-700" className="inline-flex items-center py-3 px-3 my-6 rounded tex-red-200 hover:text-green-800">
-                        About
+                    <NavLink to='/post' activeClassName="text-gray-50 bg-gray-400" className="inline-flex items-center py-3 px-3 my-6 rounded tex-red-200 hover:text-gray-50">
+                        Blog
                     </NavLink>
                 </nav>
                 <div className="inline-flex py-3 px-3 my-6">
