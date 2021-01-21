868924c5b67f6d0b74c745fc265ab4ab33f3272e
Justin Cordero <68392704+Justin-Cordero@users.noreply.github.com>
2021-01-21 16:36:19 -0500

    fix responsiveness issues

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import image from '../headerbg.jpeg';
+import React from "react";
+import image from "../headerbg.jpeg";
 
 export default function Home() {
-    return (
-        <main>
-            <img src={image} alt="Scenic River" className="absolute object-cover w-full h-full"/>
-            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-72 px-8">
-                <h1 className="text-6xl text-gray-50 text-center font-bold cursive leading-none lg:leading-snug home-name">
-                    John Smith.<br></br>Traveler & Photographer.
-                </h1>
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Scenic River"
+        className="absolute object-cover h-screen w-screen"
+      />
+      <section className="pt-36 lg:pt-72 px-8">
+        <h1 className="relative text-gray-50 text-4xl text-center font-bold cursive md:text-7xl lg:text-9xl">
+          John Smith.<br></br>Traveler & Photographer.
+        </h1>
+      </section>
+    </main>
+  );
 }
